f7610420834fdc42aed5f0105bea6a812dd2182d
Matthias Plum <matthias.plum@marquette.edu>
2022-09-07 11:55:46 -0600

    Make the template fit work for any number of input pdfs

--- template_analysis/Template_Fit.py
+++ template_analysis/Template_Fit.py
@@ -11,28 +11,28 @@ class CR_Template_Analysis:
 
     def __init__(self,minos=False):
       self.minos = minos
+      self.num_pdfs = 1
       self.pdf = None
       self.likelihood = None
       self.minuit = None
 
     def join_pdfs(self,template_pdfs):
+      ### Set number of PDFs in template analysis
+      self.num_pdfs = len(template_pdfs)
       ### Convert PDFs to extended PDFs
-      ext_pdf1 = probfit.Extended(template_pdfs[0], extname='N1')
-      ext_pdf2 = probfit.Extended(template_pdfs[1], extname='N2')
-      ext_pdf3 = probfit.Extended(template_pdfs[2], extname='N3')
-      ext_pdf4 = probfit.Extended(template_pdfs[3], extname='N4')
-      
+      pdfs_list = []
+      for i, pdf in enumerate(template_pdfs):
+        pdfs_list.append(probfit.Extended(pdf, extname='N'+str(i+1)))
+
       ### Define an extended PDF consisting of four components
-      self.pdf = probfit.AddPdf(ext_pdf1,ext_pdf2,ext_pdf3,ext_pdf4)
+      self.pdf = probfit.AddPdf(*pdfs_list)
     
     def template_binned_likelihood(self, data,set_bins,set_fitrange):
       ### Start fit-parameter
       ### Assume worst case
-      pars = dict(N1=len(data)/4,
-                  N2=len(data)/4,
-                  N3=len(data)/4,
-                  N4=len(data)/4,
-                  )
+      pars = {}
+      for i in range(self.num_pdfs):
+        pars['N'+str(i+1)]=len(data)/self.num_pdfs
 
       self.likelihood = probfit.BinnedLH(self.pdf, data, bins=len(set_bins), extended=True, bound=set_fitrange)
 
@@ -53,11 +53,9 @@ class CR_Template_Analysis:
     def template_unbinned_likelihood(self, data):
       ### Start fit-parameter
       ### Assume worst case
-      pars = dict(N1=len(data)/4,
-                  N2=len(data)/4,
-                  N3=len(data)/4,
-                  N4=len(data)/4,
-                  )
+      pars = {}
+      for i in range(self.num_pdfs):
+        pars['N'+str(i+1)]=len(data)/self.num_pdfs
 
       self.likelihood = probfit.UnbinnedLH(self.pdf, data, extended=True)
 
